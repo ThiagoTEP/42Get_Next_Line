0665da4ab47866f129e37ad80115f72c6143f09b
ThiagoTEP <thiagodepaulapt@gmail.com>
2025-06-05 11:46:07 +0100

    teste para static

---
+++ teste.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void contarcomum()
+{
+    int x = 0;
+    x++;
+    printf("x comum = %d &x = %p\n", x, &x);
+}
+
+void contastatic()
+{
+    static int x = 0;
+    x++;
+    printf("x static = %d &x = %p\n", x, &x);
+}
+
+int main(void)
+{
+    contarcomum();
+    contarcomum();
+    contarcomum();
+
+    puts("\n--------------\n");
+
+    contastatic();
+    contastatic();
+    contastatic();
+    
+    
+    return 0;
+}
